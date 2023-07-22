09ccdda2dce22c5fa47d56bfabf7a7086cf3399a
Anurag Dubey <98604732+dubey2002@users.noreply.github.com>
2023-07-22 11:24:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import trainData from './components/trainData'
+
+function App() {
+
+  return (
+    <div>
+      <TrainData />
+    </div>
+  )
+}
+
+export default App;
